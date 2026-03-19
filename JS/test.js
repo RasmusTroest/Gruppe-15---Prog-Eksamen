1b8bfc962d15a8bc74b115c4594f84806de7305b
RTR <RTR@abakion.com>
2026-03-19 18:56:35 +0100

    Test af API

--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,22 @@
 let test = [1, 2, 3, 4, 5];
 
-console.log(test);
+let endpoint = "https://api.dataforsyningen.dk/adresser?q="
+
+let adresse = "Charlotte Muncks Vej 26, 4.th."
+
+
+function makeAPIfriendly(str) {
+    return encodeURIComponent(str)
+}
+
+let URIAddress = makeAPIfriendly(adresse)
+
+let fullURI = endpoint + URIAddress
+
+console.log(fullURI)
+
+let result = await fetch(fullURI)
+
+let data = (await result.json())[0]
+
+console.log(data.adgangsadresse.vejpunkt)
