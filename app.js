2efb44899ccdd7ce871638d534a84fef3abe00ac
Rasmus Troest Stationary <rasmus_gt@outlook.dk>
2026-04-02 17:39:01 +0200

    Rework of routing  and more

--- app.js
+++ app.js
@@ -3,27 +3,44 @@ const path = require("path");
 
 const app = express();
 
-// 1) View engine
+// 1) View engine (Used for azure webapp)
 app.set("view engine", "ejs");
 
-// 2) (Optional but recommended) Set views folder explicitly
-//    Only needed if your views folder isn't exactly ./views
+// 2) Set views folder explicitly
 app.set("views", path.join(__dirname, "views"));
 
-// 3) Static files (CSS, images, JS) served from /public
+// Static files (CSS, images, JS) served from /public
 //    URL: /min.css  ->  ./public/min.css
 app.use(express.static(path.join(__dirname, "public")));
 
-// 4) Routes
+// used for setting active page in header
+app.use((req, res, next) => {
+  res.locals.currentPath = req.path;
+  next();
+});
+
+// Routes
 app.get("/", (req, res) => {
-  res.render("pages/index"); // looks for: ./views/pages/index.ejs
+  res.render("pages/dashboard"); // looks for: ./views/pages/index.ejs
 });
 
 app.get("/about", (req, res) => {
   res.render("pages/about"); // looks for: ./views/pages/about.ejs
 });
 
+app.get("/investeringscases", (req, res) => {
+  res.render("pages/investeringscases"); // looks for: ./views/pages/investeringscases.ejs
+});
+
+app.get("/minBruger", (req, res) => {
+  res.render("pages/minBruger"); // looks for: ./views/pages/minBruger.ejs
+});
+
+app.get("/sammenlignCases", (req, res) => {
+  res.render("pages/sammenlignCases"); // looks for: ./views/pages/sammenlignCases.ejs
+});
+
 // 5) Port (Azure uses process.env.PORT)
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
-``
+
